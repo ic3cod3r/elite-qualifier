611b39d54fa2ff5e3244f512654354d07ee71b92
Hunter Baker <96445786+ic3cod3r@users.noreply.github.com>
2022-01-17 14:40:32 +0000

    Added GUI, and I made the input all lower case, to correct better

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 
 import time
 #text
+
 import nltk
 from nltk.corpus import words
-from nltk.metrics.distance import (
+"""from nltk.metrics.distance import (
   #edit_distance,
   jaccard_distance,
-  )
-from nltk.util import ngrams
+  )"""
+from tkinter import *
+from textblob import TextBlob
+#from nltk.util import ngrams
 nltk.download('words')
 import pandas
 
@@ -36,6 +39,117 @@ for i in range(0,3):
   userinput.append(word)
 userinput 
 """  
+
+def clearAll() :
+     
+    # whole content of text entry area is deleted
+    word1_field.delete(0, END)
+    word2_field.delete(0, END)
+ 
+# Function to get a corrected word
+def correction() :
+ 
+    # get a content from entry box
+    input_word = word1_field.get().lower()   #I added .lower() because the autocorrect did not correct it properly if it was upper case.
+    
+  #print("One",input_word)
+    # create a TextBlob object
+    start_time = time.time()
+
+
+    #print("Two", input_word)
+    blob_obj = TextBlob(input_word)
+ 
+    # get a corrected word
+    corrected_word = str(blob_obj.correct())
+ 
+    # insert method inserting the
+    # value in the text entry box.
+    word2_field.insert(10, corrected_word)
+    end_time = time.time()
+
+    print(corrected_word)
+    elapsed_time = end_time - start_time
+    print('Corrected ' + input_word + ' in ' + f'{elapsed_time:.2f}' + ' seconds.')
+# Driver code
+if __name__ == "__main__" :
+    
+
+    # Create a GUI window
+    root = Tk()
+ 
+    # Set the background colour of GUI window
+    root.configure(background = 'black')
+     
+    # Set the configuration of GUI window (WidthxHeight)
+    root.geometry("400x150")
+ 
+    # set the name of tkinter GUI window
+    root.title("Mazz Enterprise Autocorrect")
+     
+    # Create Welcome to Spell Corrector Application: label
+    headlabel = Label(root, text = 'Welcome to Mazz Enterprise Autocorrect Application',
+                    fg = 'black', bg = "red")
+     
+    # Create a "Input Word": label
+    label1 = Label(root, text = "Input Word",
+                fg = 'black', bg = 'blue')
+         
+    # Create a "Corrected Word": label
+    label2 = Label(root, text = "Corrected Word",
+                fg = 'black', bg = 'blue')
+     
+     
+    # grid method is used for placing
+    # the widgets at respective positions
+    # in table like structure .
+    # padx keyword argument used to set padding along x-axis .
+    headlabel.grid(row = 0, column = 1)
+    label1.grid(row = 4, column = 0)
+    label2.grid(row = 10, column = 0, padx = 10)
+ 
+         
+    # Create a text entry box
+    # for filling or typing the information.
+    word1_field = Entry()
+    word2_field = Entry()
+         
+    # padx keyword argument used to set padding along x-axis .
+    # pady keyword argument used to set padding along y-axis .
+    word1_field.grid(row = 4, column = 1, padx = 10, pady = 10)
+    word2_field.grid(row = 10, column = 1, padx = 10, pady = 10)
+ 
+         
+    # Create a Correction Button and attached
+    # with correction function
+    button1 = Button(root, text = "Correction", bg = "red", fg = "black",
+                                command = correction)
+         
+    button1.grid(row = 6, column = 1)
+     
+    # Create a Clear Button and attached
+    # with clearAll function
+    button2 = Button(root, text = "Clear", bg = "red",
+                    fg = "black", command = clearAll)
+     
+    button2.grid(row = 12, column = 1)
+     
+    # Start the GUI
+    root.mainloop()
+
+
+"""
+#This is my code before the GUI, need to figure out how to make it part of my own code!!!!
+
+from textblob import TextBlob
+
+with open("text.txt", "r") as f:        # Opening the test file with the intention to read
+    text = f.read()                     # Reading the file
+    textBlb = TextBlob(text)            # Making our first textblob
+    textCorrected = textBlb.correct()   # Correcting the text
+    print(textCorrected)
+
+
 # load dictionary words from file
 def load_words():
   all_words = []
@@ -87,7 +201,7 @@ if __name__ == "__main__":
 
 
 
-
+"""
 
 
 
